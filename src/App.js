1451d494eee757dce12276225ab5efdca0f1c6e7
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-12-28 13:10:46 +0300

    started weather app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
-import TodoApp from "./projects/todoapp/TodoApp";
+// import TodoApp from "./projects/todoapp/TodoApp";
+import WeatherApp from './projects/weatherapp/WeatherApp';
 
 function App() {
   return (
     <div className="App">
-      <TodoApp />
+      {/* <TodoApp /> */}
+      <WeatherApp />
     </div>
   );
 }
